7818dffa1e8b189067bc050d06d1a9a771298b50
Victor Lopez <victor.lopez@pal-robotics.com>
2019-05-14 09:06:11 +0200

    Update PAL licenses

--- include/dynamic_introspection/dynamic_introspection.h
+++ include/dynamic_introspection/dynamic_introspection.h
@@ -1,13 +1,11 @@
-///////////////////////////////////////////////////////////////////////////////
-
-// Copyright (C) 2014, 2015 PAL Robotics S.L.
-
-// All rights reserved.
-
-//////////////////////////////////////////////////////////////////////////////
-
-// Author Hilario Tomé
-
+/*
+ * Copyright 2019 PAL Robotics SL. All Rights Reserved
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited,
+ * unless it was supplied under the terms of a license agreement or
+ * nondisclosure agreement with PAL Robotics SL. In this case it may not be
+ * copied or disclosed except in accordance with the terms of that agreement.
+ */
 #ifndef _DYNAMIC_INTROSPECTION_
 #define _DYNAMIC_INTROSPECTION_
 #include <pal_statistics/pal_statistics_macros.h>
